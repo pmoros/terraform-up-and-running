1f0f0e3e93afc47c2f5532087f69025f7c4a61c1
Paul Moros <pmoros@outlook.com>
2023-02-26 13:56:41 -0500

    feat: create lb target group for the asg

--- chapter-02/main.tf
+++ chapter-02/main.tf
@@ -68,6 +68,22 @@ resource "aws_lb_listener" "http" {
 }
 
 
+resource "aws_lb_target_group" "asg" {
+    name = "terraform-asg-example"
+    port = var.server_port
+    protocol = "HTTP"
+    vpc_id = data.aws_vpc.default.id
+    health_check {
+        path = "/"
+        protocol = "HTTP"
+        matcher = "200"
+        interval = 15
+        timeout = 3
+        healthy_threshold = 2
+        unhealthy_threshold = 2
+    }
+}
+
 resource "aws_security_group" "instance" {
     name = "terraform-example-instance"
 
