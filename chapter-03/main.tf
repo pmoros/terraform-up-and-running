a257df6503b6992a02d7875942b308f7a7d199f2
Paul Moros <pmoros@outlook.com>
2023-02-26 15:53:53 -0500

    feat: create encrypted s3_bucket for remote state

---
+++ chapter-03/main.tf
@@ -0,0 +1,33 @@
+provider "aws" {
+  region = "us-east-2"
+}
+
+# Enable versioning so we can see the full revision history of our
+# state files
+resource "aws_s3_bucket_versioning" "terraform_state" {
+  bucket = "terraform-up-and-running-state"
+
+  # Prevent accidental deletion of this S3 bucket
+  lifecycle {
+    prevent_destroy = true
+  }
+
+  # Enable versioning so we can see the full revision history of our
+  # state files
+  versioning_configuration {
+    status = "Enabled"
+  }
+
+}
+
+# Enable server-side encryption by default
+resource "aws_s3_bucket_server_side_encryption_configuration" "example" {
+  bucket = aws_s3_bucket_versioning.terraform_state.bucket
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
+
