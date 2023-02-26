5a79a09b14937d7fa1f0138d4fc9bcd9802a76cf
Paul Moros <pmoros@outlook.com>
2023-02-26 11:38:11 -0500

    feat: attach security group to the alb

--- chapter-02/main.tf
+++ chapter-02/main.tf
@@ -49,6 +49,7 @@ resource "aws_lb" "example" {
     name = "terraform-asg-example"
     load_balancer_type = "application"
     subnets = data.aws_subnets.default.ids
+    security_groups = [aws_security_group.alb.id]
 }
 
 resource "aws_lb_listener" "http" {
