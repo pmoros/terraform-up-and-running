5bc5bb1f429c5075a8dd3d197720b2dc443bacce
Paul Moros <pmoros@outlook.com>
2023-02-26 10:52:30 -0500

    refactor: replace aws_instance for aws_launch_conf

--- chapter-02/main.tf
+++ chapter-02/main.tf
@@ -13,20 +13,15 @@ output "public_ip" {
     description = "The public IP address of the web server"
 }
 
-resource "aws_instance" "example" {
-    ami = "ami-0c55b159cbfafe1f0"
+resource "aws_launch_configuration" "example" {
+    image_id = "ami-0c55b159cbfafe1f0"
     instance_type = "t2.micro"
-    vpc_security_group_ids = [aws_security_group.instance.id]
-
+    security_groups = [aws_security_group.instance.id]
     user_data = <<-EOF
                 #!/bin/bash
                 echo "Hello, World" > index.html
                 nohup busybox httpd -f -p ${var.server_port} &
                 EOF
-
-    tags = {
-        Name = "terraform-example"
-    }
 }
 
 resource "aws_security_group" "instance" {
