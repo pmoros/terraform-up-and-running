0e697e0382fd82ae8d176051cfca469308cee2b8
Paul Moros <pmoros@outlook.com>
2023-02-26 11:09:35 -0500

    feat: add autoscaling_group and data sources

--- chapter-02/main.tf
+++ chapter-02/main.tf
@@ -2,16 +2,23 @@ provider "aws" {
     region = "us-east-2"
 }
 
+data "aws_vpc" "default" {
+    default = true
+}
+
+data "aws_subnets" "default" {
+    filter {
+        name = "vpc-id"
+        values = [data.aws_vpc.default.id]
+    }
+}
+
 variable "server_port" {
     description = "The port the server will use for HTTP requests"
     type = number
     default = 8080
 }
 
-output "public_ip" {
-    value = aws_instance.example.public_ip
-    description = "The public IP address of the web server"
-}
 
 resource "aws_launch_configuration" "example" {
     image_id = "ami-0c55b159cbfafe1f0"
@@ -23,6 +30,21 @@ resource "aws_launch_configuration" "example" {
                 nohup busybox httpd -f -p ${var.server_port} &
                 EOF
 }
+
+resource "aws_autoscaling_group" "example" {
+    launch_configuration = aws_launch_configuration.example.name
+    vpc_zone_identifier = data.aws_subnets.default.ids
+
+    min_size = 2
+    max_size = 10
+
+    tag {
+        key = "Name"
+        value = "terraform-asg-example"
+        propagate_at_launch = true # 
+    }
+}
+
 
 resource "aws_security_group" "instance" {
     name = "terraform-example-instance"
