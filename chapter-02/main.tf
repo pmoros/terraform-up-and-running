81cdd94e0e05cbbde256f982379d34e048a66589
Paul Moros <pmoros@outlook.com>
2023-02-25 22:41:32 -0500

    refactor: create server_port variable

--- chapter-02/main.tf
+++ chapter-02/main.tf
@@ -2,6 +2,12 @@ provider "aws" {
     region = "us-east-2"
 }
 
+variable "server_port" {
+    description = "The port the server will use for HTTP requests"
+    type = number
+    default = 8080
+}
+
 resource "aws_instance" "example" {
     ami = "ami-0c55b159cbfafe1f0"
     instance_type = "t2.micro"
@@ -10,7 +16,7 @@ resource "aws_instance" "example" {
     user_data = <<-EOF
                 #!/bin/bash
                 echo "Hello, World" > index.html
-                nohup busybox httpd -f -p 8080 &
+                nohup busybox httpd -f -p ${var.server_port} &
                 EOF
 
     tags = {
@@ -22,8 +28,8 @@ resource "aws_security_group" "instance" {
     name = "terraform-example-instance"
 
     ingress {
-        from_port = 8080
-        to_port = 8080
+        from_port = var.server_port
+        to_port = var.server_port
         protocol = "tcp"
         cidr_blocks = ["0.0.0.0/0"]
     }
