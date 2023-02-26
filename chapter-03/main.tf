6b4862bb90d114fdef6c242b2ec4dd3cb41bb2d7
Paul Moros <pmoros@outlook.com>
2023-02-26 16:23:54 -0500

    feat: configure Terraform S3 backend

--- chapter-03/main.tf
+++ chapter-03/main.tf
@@ -1,3 +1,16 @@
+terraform {
+    backend "s3" {
+        # Replace this with your bucket name!
+        bucket = "terraform-up-and-running-state"
+        key = "global/s3/terraform.tfstate"
+        region = "us-east-2"
+        # Replace this with your DynamoDB table name!
+        dynamodb_table = "terraform-up-and-running-locks"
+        encrypt = true
+    }
+}
+
+
 provider "aws" {
   region = "us-east-2"
 }
