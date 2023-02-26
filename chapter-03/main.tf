6761808d22a26f3bc5164e9fe0ce0dbb20c632f6
Paul Moros <pmoros@outlook.com>
2023-02-26 16:38:24 -0500

    fix: bucket name in terraform backend

--- chapter-03/main.tf
+++ chapter-03/main.tf
@@ -1,7 +1,7 @@
 terraform {
     backend "s3" {
         # Replace this with your bucket name!
-        bucket = "terraform-up-and-running-state"
+        bucket = "paul-moros-terraform-up-and-running-state"
         key = "global/s3/terraform.tfstate"
         region = "us-east-2"
         # Replace this with your DynamoDB table name!
