6628e08ecf0437412fe734c3c35e2ff96c5a5787
Paul Moros <pmoros@outlook.com>
2023-02-26 16:02:53 -0500

    feat: add locks in dynamodb

--- chapter-03/main.tf
+++ chapter-03/main.tf
@@ -31,3 +31,14 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "terraform_state" 
   }
 }
 
+# Lock the state file so that only one person can modify it at a time
+resource "aws_dynamodb_table" "terraform_locks" {
+  name         = "terraform-up-and-running-locks"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
+
