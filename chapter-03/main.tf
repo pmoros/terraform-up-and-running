7908a480c6787ce2971f56577cef7c9849d9ee0f
Paul Moros <pmoros@outlook.com>
2023-02-26 17:01:21 -0500

    refactor: add backend.hcl

--- chapter-03/main.tf
+++ chapter-03/main.tf
@@ -1,12 +1,6 @@
 terraform {
   backend "s3" {
-    # Replace this with your bucket name!
-    bucket = "paul-moros-terraform-up-and-running-state"
     key    = "global/s3/terraform.tfstate"
-    region = "us-east-2"
-    # Replace this with your DynamoDB table name!
-    dynamodb_table = "terraform-up-and-running-locks"
-    encrypt        = true
   }
 }
 
