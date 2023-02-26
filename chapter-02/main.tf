7941954dd69342acd3bb934fbf6bb9e039320fd7
Paul Moros <pmoros@outlook.com>
2023-02-26 11:35:41 -0500

    feat: create security group for the alb

--- chapter-02/main.tf
+++ chapter-02/main.tf
@@ -66,6 +66,7 @@ resource "aws_lb_listener" "http" {
     }
 }
 
+
 resource "aws_security_group" "instance" {
     name = "terraform-example-instance"
 
@@ -73,6 +74,24 @@ resource "aws_security_group" "instance" {
         from_port = var.server_port
         to_port = var.server_port
         protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+}
+
+resource "aws_security_group" "alb" {
+    name = "terraform-example-alb"
+    # Allow inbound HTTP requests
+    ingress {
+        from_port = 80
+        to_port = 80
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+    # Allow all outbound requests
+    egress {
+        from_port = 0
+        to_port = 0
+        protocol = "-1"
         cidr_blocks = ["0.0.0.0/0"]
     }
 }
