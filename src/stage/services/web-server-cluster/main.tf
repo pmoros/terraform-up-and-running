ea364858aaa478e22cf5b3ace2743614a0479d65
Paul Moros <pmoros@outlook.com>
2023-02-26 20:56:13 -0500

    refactor: use file templating for user-data.sh

--- src/stage/services/web-server-cluster/main.tf
+++ src/stage/services/web-server-cluster/main.tf
@@ -34,13 +34,16 @@ resource "aws_launch_configuration" "example" {
     image_id = "ami-0c55b159cbfafe1f0"
     instance_type = "t2.micro"
     security_groups = [aws_security_group.instance.id]
-    user_data = <<EOF
-        #!/bin/bash
-        echo "Hello, World" >> index.html
-        echo "${data.terraform_remote_state.db.outputs.address}" >> index.html
-        echo "${data.terraform_remote_state.db.outputs.port}" >> index.html
-        nohup busybox httpd -f -p ${var.server_port} &
-        EOF
+    user_data = data.template_file.user_data.rendered
+}
+
+data "template_file" "user_data" {
+    template = file("user-data.sh")
+    vars = {
+        server_port = var.server_port
+        db_address = data.terraform_remote_state.db.outputs.address
+        db_port = data.terraform_remote_state.db.outputs.port
+    }
 }
 
 resource "aws_autoscaling_group" "example" {
